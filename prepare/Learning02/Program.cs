ef1152f4d2252075f33ea5843df9a760a45815e3
iabiloye <iabiloye@gmail.com>
2024-01-20 18:24:05 +0100

    job and resume cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Representative";
+        job1._company = "Nigeria Breweries";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Peer Mentor";
+        job2._company = "Bloom";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Abiloye Iyanuoluwa";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
